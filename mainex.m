d9df2c56aa55dc1da904301d12636f0c9076a288
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-01-25 17:45:17 +0100

    generalized mainex

--- mainex.m
+++ mainex.m
@@ -3,66 +3,43 @@ addpath external
 %OR use: mysetup from https://github.com/eruffaldi/matlabaddons
 % mysetup('ekfukf');
 rng(1);
-%% Define the function and compute the gradient, then an evaluable matlab function
+
+%%
+f1 = @(x) [x(2); 2*(x(1))*(x(2)+x(1))];
+f2 = @(x) [x(2)^3; 2*(x(1))*(x(2)+x(1))];
+f3 = @(x) [exp(x(2)^2); 200*(x(1)^2)*(x(2)+x(1))];
+
+ff = {f1,f2,f3};
+
+%%
 xx = sym(sym('x',[2,1]),'real');
-f = @(x) [x(2); 2*(x(1))*(x(2)+x(1))];
+x0 = [0.0054,1.01];
+P0 = diag([0.01,0.2]);
+rr = {};
+er = {};
+for I=1:length(ff)
+f = ff{I};
 fs = f(xx);
-fs1 = fs;
 
 Js = jacobian(fs); % Jacobian
 J = matlabFunction(Js,'Vars',{xx}); % Vars is required to make 1 input function
-x0 = [0.0054,1.01];
-
-P0 = diag([0.01,0.2]);
 [r,e]= compareapprox(f,J,x0,P0,500,[],'kl');
 Hm = hessianmax(fs,xx,x0);
 
 e.Hm = Hm;
+rr{I} = r;
+er{I} = e;
+end
 
-rr{1} = e;
-
-
-%% Define the function and compute the gradient, then an evaluable matlab function
-xx = sym(sym('x',[2,1]),'real');
-f = @(x) [x(2)^3; 2*(x(1))*(x(2)+x(1))];
-fs = f(xx);
-fs2 = fs;
-Js = jacobian(fs); % Jacobian
-J = matlabFunction(Js,'Vars',{xx}); % Vars is required to make 1 input function
-x0 = [0.0054,1.01];
-
-P0 = diag([0.01,0.2]);
-[r,e]= compareapprox(f,J,x0,P0,500,[],'kl');
-Hm = hessianmax(fs,xx,x0);
-
-e.Hm = Hm;
-
-rr{2} = e;
-
-%% Define the function and compute the gradient, then an evaluable matlab function
-xx = sym(sym('x',[2,1]),'real');
-f = @(x) [exp(x(2)^2); 200*(x(1)^2)*(x(2)+x(1))];
-fs = f(xx);
-fs3 = fs;
-Js = jacobian(fs); % Jacobian
-J = matlabFunction(Js,'Vars',{xx}); % Vars is required to make 1 input function
-x0 = [0.0054,1.01];
-
-P0 = diag([0.01,0.2]);
-[r,e]= compareapprox(f,J,x0,P0,500,[],'kl');
-Hm = hessianmax(fs,xx,x0);
-
-e.Hm = Hm;
-
-rr{3} = e;
-displayapprox(r,1,1);
+%%
+displayapprox(rr{3},1,1);
 
 
 %%
 %
-D = zeros(length(rr),3);
-for I=1:length(rr)
-    D(I,:) = [rr{I}.Hm,rr{I}.y.ds.linear(1),rr{I}.y.ds.ut(1)];
+D = zeros(length(er),3);
+for I=1:length(er)
+    D(I,:) = [er{I}.Hm,er{I}.y.ds.linear(1),er{I}.y.ds.ut(1)];
 end
 ds = dataset([D,{'H','EKF','UT'}],'Obs',{'f1','f2','f3'});
 ds
